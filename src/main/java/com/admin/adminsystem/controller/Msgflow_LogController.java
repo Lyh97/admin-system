2b7449ac0bf1c9b378dc4e4544c01f0de75e5b23
Lyh97 <18524637652@163.com>
2018-07-08 12:57:48 +0800

    test

--- src/main/java/com/admin/adminsystem/controller/Msgflow_LogController.java
+++ src/main/java/com/admin/adminsystem/controller/Msgflow_LogController.java
@@ -120,7 +120,7 @@ public class Msgflow_LogController {
     }
 
     /**
-     * Select by serial number json object.
+     * Select By serial number json object.
      *
      * @param info the info
      * @return the json object
